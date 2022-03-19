575dff3b185fe4b9fa4fcd20a873990a15fa1f1b
id-isymhrk <isymhrk0608@gmail.com>
2022-03-19 13:17:00 +0900

    背景色とフォント名を変更

--- ChangeScene/src/Password.cpp
+++ ChangeScene/src/Password.cpp
@@ -1,8 +1,11 @@
 ﻿#include"AllScene.hpp"
 
 Password::Password(const InitData& init)
-	:IScene(init), font15(15), font25(25), box_text(Arg::center(400, 500), 400, 30)
+	:IScene(init), font_guide(15), font_input(25), box_text(Arg::center(400, 500), 400, 30)
 {
+	// 背景の色を設定 | Set background color
+	Scene::SetBackground(Palette::Black);
+
 	ClearPrint();
 }
 
@@ -24,17 +27,17 @@ void Password::update() {
 
 void Password::draw() const {
 	box_text.draw(Color(255, 255, 255));
-	font15(U"password:").drawAt(p.movedBy(-220, -30), Palette::White);
-	font15(U"[delete]:show / delete").draw(p.movedBy(50, 20), Palette::White);
-	font15(U"[Enter]:finish").draw(p.movedBy(50, 40), Palette::White);
+	font_guide(U"password:").drawAt(p.movedBy(-220, -30), Palette::White);
+	font_guide(U"[delete]:show / delete").draw(p.movedBy(50, 20), Palette::White);
+	font_guide(U"[Enter] : finish").draw(p.movedBy(50, 40), Palette::White);
 
 	//パスワードを 隠す/隠さない 処理
 	if (not flag_hide) {
-		font25(text_input).draw(205, 482, Color(0, 0, 0));
+		font_input(text_input).draw(205, 482, Color(0, 0, 0));
 	}
 	else {
 		for (size_t i = 0; i < text_input.size(); i++) {
-			font25(U"●").draw(205 + 25 * i, 482, Color(0, 0, 0));
+			font_input(U"●").draw(205 + 25 * i, 482, Color(0, 0, 0));
 		}
 	}
 }
